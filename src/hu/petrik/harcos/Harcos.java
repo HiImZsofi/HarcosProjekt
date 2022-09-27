14341a600f4444a4c40390f1702d651538503f2d
GyZsofi <zsofia.gyori00@gmail.com>
2022-09-27 19:08:06 +0200

    constructor and getter methods

---
+++ src/hu/petrik/harcos/Harcos.java
@@ -0,0 +1,43 @@
+package hu.petrik.harcos;
+
+public class Harcos {
+    private String nev;
+    private int szint;
+    private int tapasztalat;
+    private int eletero;
+    private int alapEletero;
+    private int alapSebzes;
+
+
+    public Harcos(String nev, int statuszSablon){
+        this.nev = nev;
+        this.szint = 1;
+        this.tapasztalat = 0;
+        if(statuszSablon == 1){
+            alapEletero = 15;
+            alapSebzes = 3;
+        }
+        else if(statuszSablon == 2){
+            alapEletero = 12;
+            alapSebzes = 4;
+        }
+        else if(statuszSablon == 3){
+            alapEletero = 8;
+            alapSebzes = 5;
+        }
+    }
+
+    public int getSebzes() {
+        return alapSebzes + szint;
+    }
+
+    public int getSzintLepeshez(){
+        return 10 + szint*5;
+    }
+
+    public int getMaxEletero(){
+        return alapEletero + szint * 3;
+    }
+
+
+}
