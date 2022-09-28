ac06fc7df5d3f388acff31f06331c6f01551690c
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-28 10:03:31 +0200

    file reader method

--- src/hu/petrik/harcos/Main.java
+++ src/hu/petrik/harcos/Main.java
@@ -1,7 +1,28 @@
 package hu.petrik.harcos;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Main {
+    static List<Harcos> harcosok = new ArrayList<>();
+    public static void main(String[] args) {
+        File file = new File("harcosok.csv");
+        Scanner scR;
+        try {
+            scR = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (scR.hasNextLine()) {
+            String[] temp = scR.nextLine().split(";");
+            harcosok.add(new Harcos(temp[0], Integer.parseInt(temp[1])));
+        }
+        for (Harcos harcos : harcosok) {
+            System.out.println(harcos.toString());
+        }
     }
 }
